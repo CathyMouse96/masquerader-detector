dc6cad46472e607af1315b36b18c85459a763463
Qing Teng <qt2126@columbia.edu>
2018-11-30 00:07:56 -0500

    Add code for loading data

--- train.py
+++ train.py
@@ -1,18 +1,44 @@
 """ Trains a GMM based on user behavior data. """
 
-# Created by CMouse (qt2126@columbia.edu)
+""" ---- Created by CMouse (qt2126@columbia.edu) ---- """
+
+import argparse
+import logging
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 from sklearn.mixture import GaussianMixture
 
-# Create GMM
-gmm = GaussianMixture(n_components=10)
-# Train GMM
-gmm.fit(digits.data)
-# Save GMM
-test_scores = gmm.score_samples(digits.data)
-plt.plot(test_scores)
-plt.title("Scores")
-plt.show()
+import utils
+
+logging.basicConfig(level=logging.INFO)
+
+def parse_args():
+    """ Parse command line arguments. """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--data_dir', default='data', type=str, 
+        help='The input data directory.')
+    parser.add_argument('-m', '--model_dir', default='model', type=str,
+        help='The output model directory')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+
+    # Input data is numpy array with dimensions n_samples * n_features
+    data = utils.load_data(args.data_dir)
+
+    # Create GMM
+    gmm = GaussianMixture(n_components=1)
+
+    # Train GMM
+    gmm.fit(data)
+
+    # Save GMM
+    test_scores = gmm.score_samples(data)
+    plt.plot(test_scores)
+    plt.title("Scores")
+    plt.show()
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+import numpy as np
+
+def load_data(path):
+    """ Load and combine .log files at specific path. """
+    logging.info("Loading data at {}...".format(path))
+    data = []
+    datalen = 0
+    for filename in os.listdir(path):
+        if filename.endswith(".log"):
+            with open(os.path.join(path, filename), 'r') as f:
+                for line in f.readlines():
+                    data.append([int(x) for x in line.strip()[1:-1].split(', ')])
+                    datalen += 1
+    logging.info("Loaded {} samples from {}.".format(datalen, path))
+    return np.array(data)
